bf0f51ccd2ce5a5a4ecc9a09db94eb4f5a51a0ec
Peter Arany <aranypeter20@gmail.com>
2023-05-02 20:37:15 +0200

    gcode

--- src/gcode_writer.cpp
+++ src/gcode_writer.cpp
@@ -4,10 +4,30 @@ gcode_writer::gcode_writer(const std::string &output_name) : file_writer(output_
 {
 }
 
-void gcode_writer::write_gcode(const sliced_object *obj)
-{
-}
-
 void gcode_writer::print_polylines(const polylines &p)
 {
+    for (unsigned int i = 0; i < p.data.size(); i++)
+    {
+        for (unsigned int j = 0; j < p.data[i].size(); j++)
+        {
+            if (j == 0)
+            {
+                file_writer << "G00 X" << p.data[i][j].x << " Y" << p.data[i][j].y << std::endl;
+            }
+            else
+            {
+                file_writer << "G01 X" << p.data[i][j].x << " Y" << p.data[i][j].y << std::endl;
+            }
+        }
+    }
+}
+
+void gcode_writer::write_gcode(const sliced_object *obj)
+{
+    file_writer << "%" << std::endl;
+    for (unsigned int i = 0; i < obj->get_slice_count(); i++)
+    {
+        print_polylines(obj->get_poly_level(i));
+    }
+    file_writer << "%" << std::endl;
 }
--- src/gcode_writer.h
+++ src/gcode_writer.h
@@ -10,6 +10,10 @@ class gcode_writer
 
     void print_polylines(const polylines &);
 
+    // [mm/min]
+    const float printing_speed = 50.0f;
+    const float replace_speed = 400.0f;
+
 public:
     gcode_writer(const std::string &output_name);
 
