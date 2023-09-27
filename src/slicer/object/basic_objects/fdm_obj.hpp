8ff02e3b8c2f82dd6dca8aae0726b67d371fa9b8
aranyp20 <aranypeter20@gmail.com>
2023-09-27 17:37:43 +0200

    fdm_obj

---
+++ src/slicer/object/basic_objects/fdm_obj.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "frep.hpp"
+#include "frep_scale.hpp"
+#include "frep_invert.hpp"
+#include "frep_intersect.hpp"
+
+class fdm_obj : public frep_object
+{
+    const frep_object &original;
+
+    const frep_scale &offset_obj;
+
+    const frep_invert &offset_inverted;
+
+    const frep_intersect &wall;
+
+
+public:
+    fdm_obj(const frep_object &_original, float offset) : original(_original), offset_obj(frep_scale(original, -0.2)), offset_inverted(frep_invert(offset_obj)), wall(frep_intersect(original,offset_inverted)){}
+
+    interval fn(const interval &X, const interval &Y, double h) const override
+    {
+        return wall.fn(X,Y,h);
+    }
+    double fn(const vec3 &p) const override
+    {
+        return wall.fn(p);
+    }
+    dnum fn(const dnum &X, const dnum &Y, const dnum &h) const override
+    {
+        return wall.fn(X,Y,h);
+    }
+};
