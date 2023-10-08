c7dff8afaba04b1d9914f29e91dbbd27d016ae8b
Peter Arany <aranypeter20@gmail.com>
2023-10-08 12:53:48 +0200

    bunny size fix

--- src/gui/main_window.cpp
+++ src/gui/main_window.cpp
@@ -90,51 +90,55 @@ void main_window::update_sliced_views()
     ui->widget_2->update();
 }
 
-
-std::array<Geometry::Point3D, 2> boundingBox(const Geometry::TriMesh &mesh) {
-  Geometry::Point3D boxmin, boxmax;
-  const auto &points = mesh.points();
-  boxmin = boxmax = points[0];
-  for (const auto &p : points)
-    for (int i = 0; i < 3; ++i) {
-      boxmin[i] = std::min(boxmin[i], p[i]);
-      boxmax[i] = std::max(boxmax[i], p[i]);
-    }
-  // Add 5%
-  auto mean = (boxmin + boxmax) / 2;
-  boxmin = mean + (boxmin - mean) * 1.05;
-  boxmax = mean + (boxmax - mean) * 1.05;
-  return { boxmin, boxmax };
+std::array<Geometry::Point3D, 2> boundingBox(const Geometry::TriMesh &mesh)
+{
+    Geometry::Point3D boxmin, boxmax;
+    const auto &points = mesh.points();
+    boxmin = boxmax = points[0];
+    for (const auto &p : points)
+        for (int i = 0; i < 3; ++i)
+        {
+            boxmin[i] = std::min(boxmin[i], p[i]);
+            boxmax[i] = std::max(boxmax[i], p[i]);
+        }
+    // Add 5%
+    auto mean = (boxmin + boxmax) / 2;
+    boxmin = mean + (boxmin - mean) * 1.05;
+    boxmax = mean + (boxmax - mean) * 1.05;
+    return {boxmin, boxmax};
 }
 
-std::array<size_t, 3> computeResolution(const std::array<Geometry::Point3D, 2> &bbox, size_t size) {
-  std::array<size_t, 3> resolution;
-  auto axis = bbox[1] - bbox[0];
-  double axis_delta = axis.norm() / size / std::sqrt(3);
-  resolution[0] = std::max<size_t>((size_t)std::ceil(axis[0] / axis_delta), 2);
-  resolution[1] = std::max<size_t>((size_t)std::ceil(axis[1] / axis_delta), 2);
-  resolution[2] = std::max<size_t>((size_t)std::ceil(axis[2] / axis_delta), 2);
-  return resolution;
+std::array<size_t, 3> computeResolution(const std::array<Geometry::Point3D, 2> &bbox, size_t size)
+{
+    std::array<size_t, 3> resolution;
+    auto axis = bbox[1] - bbox[0];
+    double axis_delta = axis.norm() / size / std::sqrt(3);
+    resolution[0] = std::max<size_t>((size_t)std::ceil(axis[0] / axis_delta), 2);
+    resolution[1] = std::max<size_t>((size_t)std::ceil(axis[1] / axis_delta), 2);
+    resolution[2] = std::max<size_t>((size_t)std::ceil(axis[2] / axis_delta), 2);
+    return resolution;
 }
 
 void approximateNormals(std::vector<IPIA::PointNormal> &pns,
-                        const std::list<Geometry::TriMesh::Triangle> &tris) {
-  // Weights according to:
-  //   N. Max, Weights for computing vertex normals from facet normals.
-  //     Journal of Graphics Tools, Vol. 4(2), 1999.
-  for (const auto &tri : tris) {
-    for (size_t i = 0; i < 3; ++i) {
-      size_t i0 = tri[i], i1 = tri[(i+1)%3], i2 = tri[(i+2)%3];
-      Geometry::Vector3D v1 = pns[i0].p - pns[i2].p, v2 = pns[i1].p - pns[i0].p;
-      double w = v1.normSqr() * v2.normSqr();
-      pns[i0].n += (v1 ^ v2) / (w == 0.0 ? 1.0 : w);
+                        const std::list<Geometry::TriMesh::Triangle> &tris)
+{
+    // Weights according to:
+    //   N. Max, Weights for computing vertex normals from facet normals.
+    //     Journal of Graphics Tools, Vol. 4(2), 1999.
+    for (const auto &tri : tris)
+    {
+        for (size_t i = 0; i < 3; ++i)
+        {
+            size_t i0 = tri[i], i1 = tri[(i + 1) % 3], i2 = tri[(i + 2) % 3];
+            Geometry::Vector3D v1 = pns[i0].p - pns[i2].p, v2 = pns[i1].p - pns[i0].p;
+            double w = v1.normSqr() * v2.normSqr();
+            pns[i0].n += (v1 ^ v2) / (w == 0.0 ? 1.0 : w);
+        }
     }
-  }
-  for (auto &pn : pns)
-    if (pn.n.norm() > Geometry::epsilon)
-      pn.n.normalize();
+    for (auto &pn : pns)
+        if (pn.n.norm() > Geometry::epsilon)
+            pn.n.normalize();
 }
-
 
 void main_window::load_object()
 {
@@ -152,20 +156,19 @@ void main_window::load_object()
     size_t n = mesh.points().size();
     samples.resize(n);
     for (size_t i = 0; i < n; ++i)
-        samples[i] = { mesh[i], { 0, 0, 0 } };
+        samples[i] = {mesh[i], {0, 0, 0}};
 
     approximateNormals(samples, mesh.triangles());
-  
+
     double step = 0.001;
     size_t iterations = 20;
     double tol = 0.00001;
     size_t done_iterations = surface.fit(samples, step, iterations, tol);
 
-    auto eval = [&](const DualContouring::Point3D &p) { return surface({ p[0], p[1], p[2] }); };
-    std::array<DualContouring::Point3D, 2> dc_bbox = { {
-        { bbox[0][0], bbox[0][1], bbox[0][2] },
-        { bbox[1][0], bbox[1][1], bbox[1][2] }
-    } };
+    auto eval = [&](const DualContouring::Point3D &p)
+    { return surface({p[0] / 10, p[1] / 10, p[2] / 10}); };
+    std::array<DualContouring::Point3D, 2> dc_bbox = {{{bbox[0][0] * 10, bbox[0][1] * 10, bbox[0][2] * 10},
+                                                       {bbox[1][0] * 10, bbox[1][1] * 10, bbox[1][2] * 10}}};
     auto dc_mesh = DualContouring::isosurface(eval, 0.0, dc_bbox, dc_res);
 
     std::shared_ptr<DualContouring::QuadMesh> qm = std::make_shared<DualContouring::QuadMesh>(dc_mesh);
