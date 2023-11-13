25851ebc824d868589712d430114255e73566cea
Peter Arany <aranypeter20@gmail.com>
2023-11-13 18:35:29 +0100

    periodic support cut

--- CMakeFiles/mkultra_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mkultra_autogen"
-  "mkultra_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mkultra_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
